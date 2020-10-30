d089d36bb9ef2b0a3fca7f2e420d3dc4534411d9
Janos Peter Toth <tojape@gmail.com>
2020-10-30 15:02:42 +0100

    Added micropython intitializing code which enables to use the power on button. Also some motor controller initialization code was included. The code assumes that the ESP32 micropython firmware was previously installed on the module.

--- Software/Micropython/robotond2020_10_init.py
+++ Software/Micropython/robotond2020_10_init.py
@@ -1,7 +1,7 @@
 from machine import Pin
-led=Pin(2,Pin.OUT)
-pwr=Pin(27,Pin.OUT)
-M1A=Pin(32,Pin.OUT)
+led=Pin(2,Pin.OUT) #onboard LED
+pwr=Pin(27,Pin.OUT) #PWR_ON pin
+M1A=Pin(32,Pin.OUT) #Motor conrtoller pins as appear on the motor controller board
 M1B=Pin(33,Pin.OUT)
 M2A=Pin(25,Pin.OUT)
 M2B=Pin(26,Pin.OUT)
